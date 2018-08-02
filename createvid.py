ee9d8b2f6892739b1acb272bbd76b5bb47c3a43a
arpitsri3 <i.arpitsri@gmail.com>
2018-08-02 07:15:08 +0000

    Added output videos

--- createvid.py
+++ createvid.py
@@ -1,3 +1,4 @@
+from moviepy.editor import VideoFileClip
 import numpy as np
 import cv2
 import argparse
@@ -29,6 +30,44 @@ countFile = args.countFile
 feed_vid = cv2.VideoCapture(vid_path)
 success = True
 
+fps = feed_vid.get(cv2.CAP_PROP_FPS)
+
+fps = np.int32(fps)
+print("Frames Per Second:",fps,"\n")
+
+counter = 0
+tm = 1
+
+out = open(countFile, "r")
+
+
+def pipeline(img):
+    global counter, tm
+    counter += 1
+    txt = out.readline(tm)
+    if counter <= fps:
+        cv2.putText(img, txt, (0, 0), font, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+    else:
+        cv2.putText(img, txt, (0, 0), font, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+        counter = 0
+        tm += 1
+    return img
+
+
+video_output = out_path #'./test_videos_output/project_video_output.mp4'
+## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+## To do so add .subclip(start_second,end_second) to the end of the line below
+## Where start_second and end_second are integer values representing the start and end of the subclip
+## You may also uncomment the following line for a subclip of the first 5 seconds
+##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+clip1 = VideoFileClip(vid_path)
+out_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
+out_clip.write_videofile(video_output, audio=False)
+
+"""
+feed_vid = cv2.VideoCapture(vid_path)
+success = True
+fourcc = cv2.VideoWriter_fourcc(*'H264')
 if success:
     success, im = feed_vid.read()
     print("success-", success)
@@ -41,7 +80,7 @@ if success:
     counter = 0
     tm = 1
     if success:
-        video = cv2.VideoWriter(out_path, -1, 1, (im.shape[1], im.shape[0]))
+        video = cv2.VideoWriter(out_path, cv2.VideoWriter_fourcc(*'H264'), fps, (im.shape[1], im.shape[0]))
 
     out = open(countFile, "r")
 
@@ -57,8 +96,9 @@ if success:
             tm += 1
         video.write(img)
         success, im = feed_vid.read()
-
+"""
 out.close()
-video.release()
+#video.release()
 feed_vid.release()
 cv2.destroyAllWindows()
+
